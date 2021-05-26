982e9205a74c658b268912acabcc2cb7e977abb7
teionhozonn <teionhozonn@gmail.com>
2021-05-26 20:09:13 +0900

    0526

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::resource('posts', [PostController::class])->only(['index', 'show']);
